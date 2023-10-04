3d8b3b2d1617adcae8c15f91c28542db0f74a83f
Ronald Delgado <b.boy2019ronald@gmail.com>
2023-10-03 19:58:15 -0600

    mejora reto4

--- Challenges/Challenge_4/index.js
+++ Challenges/Challenge_4/index.js
@@ -1,129 +1,81 @@
-/*Seleccionamos el boton submit*/
-const claim = document.getElementById("claim");
 
-/*Seleccionamos cada imput por su orden*/
-const first_name = document.getElementById("first_name");
+document.addEventListener("DOMContentLoaded", function () {
+    const firstcontainer = document.querySelector(".firstcontainer");
+    const container = document.querySelector(".container");
+    const name = document.getElementById("text");
+    const form = document.getElementById("registro-form");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const modal = document.getElementById("modal");
+    const modal_boton = document.querySelector(".modal_boton");
 
-const last_name = document.getElementById("last_name");
+    // PRIMERA SECCION DE LA APP 
+    const input_name = document.getElementById("input_name");
+    const submit_name = document.getElementById("submit_name");
+    const container__p = document.querySelector(".container__p");
 
-const email = document.getElementById("email");
-
-const password = document.getElementById("password");
-
-
-
-
-/*Seleccionamos cada alerta */
-
-const alert_name = document.querySelector(".alert_name");
-
-const alert_lastname = document.querySelector(".alert_lastname");
-
-const alert_email = document.querySelector(".alert_email");
-
-const alert_password = document.querySelector(".alert_password");
-/*Realizamos la funcionalidad*/
-
-claim.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    validateEmpty(first_name.value);
-    validateEmpty2(last_name.value);
-    validate_email(email.value);
-    validate_password(password.value);
-
+   // FUNCION DE VALIDACION DE INGRESO DE NOMBRE
    
+   submit_name.addEventListener("click", ()=> {
+     let name_value = input_name.value;
 
-    // Validamos cada campo y aumentamos z si es válido.
-    if (validateEmpty(first_name.value)) {
-        z++;
-    }
+     if (name_value === "") {
+        alert("ingresa un nombre de lo contrario no puedes avanzar");
+     }else{
+        firstcontainer.style.display="none";
+        container.style.display= "block";
+        container__p.textContent = name_value;
 
-    if (validateEmpty2(last_name.value)) {
-        z++;
-    }
+     }
 
-    if (validate_email(email.value)) {
-        z++;
-    }
-
-    if (validate_password(password.value)) {
-        z++;
-    }
-
-    // Si todos los campos son válidos (z llega a 4), mostramos la alerta de éxito.
-    if (z === 4) {
-        alert("Se ha registrado correctamente.");
-    }
+   });
 
 
 
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        const namePattern = /^[a-zA-Z\s]+$/;
+        
+        const nameValue = name.value;
+        const emailValue = email.value;
+        const passwordValue = password.value;
+        
+
+        
+
+        if (!namePattern.test(nameValue)) {
+            alert("Debes ingresar un nombre válido.");
+            return;
+        }
+
+        if (!emailPattern.test(emailValue)) {
+            alert("Debes ingresar un email válido.");
+            return;
+        }
+        
+        if (!passwordPattern.test(passwordValue)) {
+            alert("Debes crear una contraseña válida.");
+            return;
+        }
+        
+        if (emailPattern.test(emailValue) && passwordPattern.test(passwordValue)) {
+            modal.showModal();
+            email.value = "";
+            password.value = "";
+            name.value = "";
+        }
+
+        modal_boton.addEventListener("click", ()=>{
+            modal.close();
+            container.style.display="none";
+            firstcontainer.style.display="block";
+            input_name.value= "";
+        })
+    });
 });
 
-let z = 0;
 
-function validateEmpty(valueInput) {
-    if (valueInput.length == 0) {
-        show_error();
-    } else {
-        eliminate_error();
-    }
-}
-function validateEmpty2(value_lastname) {
-    if (value_lastname.length == 0) {
-        show_error2();
-    } else {
-        eliminate_error2();
-    }
-}
-function show_error() {
-    alert_name.style.display = "flex";
-    first_name.style.border = "1px solid #ff7a7a";
-}
-function eliminate_error() {
-    alert_name.style.display = "none";
-    first_name.style.border = "1px solid #38cc8c";
-    z++;
-}
-function show_error2() {
-    alert_lastname.style.display = "flex";
-    last_name.style.border = "1px solid #ff7a7a";
-}
-function eliminate_error2() {
-    alert_lastname.style.display = "none";
-    last_name.style.border = "1px solid #38cc8c";
-    z++;
-}
-
-
-
-function validate_email(emailvalue) {
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-    if (!emailRegex.test(emailvalue)) {
-        email.style.border = "1px solid #ff7a7a";
-        alert_email.style.display = "flex";
-
-    } else {
-        email.style.border = "1px solid #38cc8c";
-        alert_email.style.display = "none";
-        z++;
-    }
-
-}
-
-function validate_password(passwordvalue) {
-    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-    if (passwordRegex.test(passwordvalue) == true) {
-        password.style.border = "1px solid #38cc8c";
-        alert_password.style.display = "none";
-        z++;
-
-    } else {
-        password.style.border = "1px solid #ff7a7a";
-        alert_password.style.display = "flex";
-    }
-
-}
 
